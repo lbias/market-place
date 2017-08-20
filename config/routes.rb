aefd9947b796c5e9108074bc3ef421a6690c1208
lbias <306508354@qq.com>
2017-08-20 23:32:52 +0800

    scope the routes to include the version on the url

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 MarketPlace::Application.routes.draw do
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/'  do
+    scope module: :v1 do
+    end
   end
 end
